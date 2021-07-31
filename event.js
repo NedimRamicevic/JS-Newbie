3b595b819066e3ab868138c990b8bdb4efb0912a
Nedim <mr.ramicevic@gmail.com>
2021-07-31 12:12:50 +0300

    mouseEvents

--- event.js
+++ event.js
@@ -1,3 +1,14 @@
-    function onClick(e){
-    e.parentNode.style.backgroundColor = "red"
+function onClick(e) {
+  e.parentNode.style.backgroundColor = "red";
+}
+function mouseOver(node) {
+  node.parentNode.style.backgroundColor = "purple";
+}
+function mouseLeave(node) {
+  node.parentNode.style.backgroundColor = "blue";
+  console.log("node : " + node);
+  node.parentNode.addEventListener("mouseleave", (e) => {
+    e.target.style.backgroundColor = "red";
+    console.log("e : " + e.target);
+  });
 }
